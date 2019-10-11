21f43ed3a0a9f81f64bbed05fd898b3282af2f22
Imnotbob <Imnot_bob@hotmail.com>
2019-10-11 11:16:54 -0400

    update for schedules

--- smartapps/ady624/webcore-piston.src/webcore-piston.groovy
+++ smartapps/ady624/webcore-piston.src/webcore-piston.groovy
@@ -18,7 +18,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Last update October 10, 2019 for Hubitat
+ * Last update October 11, 2019 for Hubitat
 */
 public static String version() { return "v0.3.110.20191009" }
 public static String HEversion() { return "v0.3.110.20191009_HE" }
@@ -3042,7 +3042,7 @@ private long vcmd_clearTile(Map rtData, device, List params) {
 }
 
 private long vcmd_setLocationMode(Map rtData, device, List params) {
-	String  modeIdOrName = (String)params[0]
+	String modeIdOrName = (String)params[0]
 	def mode = location.getModes()?.find{ (hashId((long)it.id) == modeIdOrName) || ((String)it.name == modeIdOrName)}
 	if(mode) {
 		location.setMode((String)mode.name)
@@ -4435,12 +4435,13 @@ private boolean evaluateComparison(Map rtData, String comparison, Map lo, Map ro
 
 private void cancelStatementSchedules(Map rtData, int statementId, String data = (String)null) {
 	//cancel all schedules that are pending for statement statementId
-	if((int)rtData.logging > 2) debug "Cancelling statement #${statementId}'s schedules...", rtData
+	boolean found = false
 	for(Map item in (List)rtData.cancelations.statements) {
-		if(!(statementId  == (int)item.id && data == (String)item.data)) {
-			((List)rtData.cancelations.statements).push([id: statementId, data: data])
-		}
+		found = (statementId == (int)item.id && (!data || data == (String)item.data))
+		if(found) break
 	}
+	if((int)rtData.logging > 2) debug "Cancelling statement #${statementId}'s schedules...", rtData
+	if(!found) ((List)rtData.cancelations.statements).push([id: statementId, data: data])
 }
 
 private void cancelConditionSchedules(Map rtData, int conditionId) {
